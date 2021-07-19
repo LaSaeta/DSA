5941c0b3f4f49efa4eb27931acb942226eb9997e
Satya <satyap48@gmail.com>
2021-07-19 07:54:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 5, 5, 5, 5, 5, 5, 5 };
+            int maxElement = Array.MaxElement(input);
+            int minElement = Array.MinElement(input);
+            int[] minAndMaxElement = Array.MinAndMax(input);
+            Console.WriteLine(minElement);
+        }
+    }
+}
